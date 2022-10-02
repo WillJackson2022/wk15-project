d87c0acb58df8ec3c172bc5e4879cd19d2d98e71
William Jackson <108308676+WillJackson2022@users.noreply.github.com>
2022-10-02 09:31:03 -0400

    Create create_sqs_queue.py

---
+++ create_sqs_queue.py
@@ -0,0 +1,8 @@
+# Get the service resource
+sqs = boto3.resource('sqs')
+
+# Create the queue. This returns an SQS.Queue instance
+queue = sqs.create_queue(QueueName='queue-name')
+
+# You can now access identifiers and attributes
+print(queue.url)
